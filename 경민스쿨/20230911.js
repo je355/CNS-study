c97a844e331242a24b533c7f7f00475e3a735eaa
je355 <120763503+je355@users.noreply.github.com>
2023-09-11 23:19:17 +0900

    경민스쿨 0911
    경민스쿨 0911 자바 콘솔 수업

---
+++ 경민스쿨/20230911.js
@@ -0,0 +1,22 @@
+// let a = [1, 2, 3];
+
+// a.forEach((value, index)=>console.log(value, index));
+// a.map((value, index)=>console.log(value, index));
+
+// a.forEach((value, index)=>value*2);
+// a.map((value, index)=>value*2);
+
+//
+
+// let a = [10, 5, 6, 3, 1, 9];
+// let b = a.filter
+// let b = a.filter((value)=>value>5);
+// //a란 값을 하나하나 불러서 조건 충족시 b 넣기 
+
+// let test = ['user1', 'user2', 'user3'];
+// test.push('user2');
+// //3번째라서 3
+
+// setTimeout(()=>{
+//     console.log("2초 뒤 실행");
+// }, 2000);
