8df4fc1dca095b2f7ea7ce164688735916e22ab2
je355 <120763503+je355@users.noreply.github.com>
2023-10-19 23:43:28 +0900

    로그인구현

---
+++ 경민스쿨/src/App.js
@@ -0,0 +1,14 @@
+// import Header from "./Header";
+
+function App() {
+  return (
+    <>
+    {/* <Header/> */}
+    {/* <Header></Header> */}
+      <div></div>
+      <p>안녕하세요</p>
+    </>
+  );
+}
+
+export default App;
---
+++ 경민스쿨/src/Header.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import "./Header.css";
+import { useNavigate } from 'react-router-dom';
+
+const Header = () => {
+  const Navigate = useNavigate();
+  return (
+    <div className='Container MainHeader'>
+        <div className='Container'>
+            <div className='Logo' onClick={()=>{Navigate("/");
+          }}>Logo</div>
+            <div className='Container Context'>
+                <p onClick={()=>{Navigate("/");
+          }}>홈</p>
+          <p onClick={()=>{Navigate("/detail");
+          }}>디테일</p>
+                {/* <p>원서접수</p>
+                <p>공지사항</p> */}
+            </div>
+        </div>
+        <div className='Container Button'>
+            <button onClick={()=>{Navigate("/login");
+          }}>로그인</button>
+            {/* <button>회원가입</button> */}
+        </div>
+    </div>
+  );
+};
+
+export default Header
+//rafce 선언 시 생성ㅇ
